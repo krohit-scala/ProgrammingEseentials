a96a7429c1168406b8e066496e76d98822639918
krohit-scala <rohit_sun_india@yahoo.co.in>
2022-03-25 01:13:18 +0530

    ReverseLinkedListInPairs + ReorderLinkedList

--- src/main/scala/dataStructures/impl/linkedList/ReorderLinkedListV2.scala
+++ src/main/scala/dataStructures/impl/linkedList/ReorderLinkedListV2.scala
@@ -4,18 +4,80 @@ package dataStructures.impl.linkedList
 // Modify it, without using extra space as, L1 = {A1, An, A2, An-1, A3, ...}
 
 object ReorderLinkedListV2 {
+  // Get new LinkedList of size N
+  // Nodes values from 1 to N
+  def getLinkedList(n: Int) : LinkedList = {
+    // Base check
+    if(n < 1)
+      return null
 
-  def reorderLinkedList : Unit = {
-    val arr = 1.to(10).toArray
+    val arr = 1.to(n).toArray
     val ll = new LinkedList()
 
     // Populate the LinkedList
     ll.appendArray(arr)
+    ll
+  }
+
+  // Brute force I feel that's cheating
+  // Swapping data (not Nodes) in pairs
+  def reverseInPairsSwappingValues: Unit = {
+    val ll = this.getLinkedList(5)
+    val head : Node = ll.head
+    var curr : Node = head
+    while(curr != null && curr.next != null){
+      val next : Node = curr.next
+      val temp = curr.data
+      curr.data = next.data
+      next.data = temp
+
+      curr = curr.next.next
+    }
+    ll.printList
+  }
+
+  // Swapping Nodes in pairs in a LinkedList
+  def reverseInPairsSwappingNodes(_head: Node): Node = {
+    // Base checks
+    if(_head == null || _head.next == null)
+      return _head
+
+    // References preparation
+    var head : Node = _head         // Points to the head of the LinkedList
+    val left : Node = _head         // 1st Node of the LinkedList
+    val right : Node = _head.next   // 2nd Node of the LinkedList
+    val tail : Node = right.next    // Rest of the LinkedList Nodes after 2nd Node
+
+    head = right        // Now head points the 2nd Node
+    right.next = left   // Now 2nd Node points the 1st Node
+
+    // Make the 1st Node point to the tail (rest of the reversed in pair LinkedList)
+    left.next = reverseInPairsSwappingNodes(tail)
+    head
+  }
+
+  // Swaps the Nodes of the LinkedList in pairs
+  def swapNodesInPairs : Unit = {
+    // Populate LinkedList
+    val ll = this.getLinkedList(9)
+    println(s"Before swapping...")
+    ll.printList
+    val head = ll.head
+    ll.head = reverseInPairsSwappingNodes(head)
+    println(s"After swapping...")
+    ll.printList
+  }
+
+  // Given a LinkedList, L1 = {A1, A2, A3, ..., An}
+  // Modify it, without using extra space as, L1 = {A1, An, A2, An-1, A3, ...}
+  def reorderLinkedList : Unit = {
+    // Get LinkedList
+    val ll : LinkedList = getLinkedList(5)
 
     // Step-1: Divide in halves
     val temp = ll.divideInHalves
-    var l1 = temp._1
-    var l2 = temp._2
+    val l1 = temp._1
+    val l2 = temp._2
 
     // Testing
     // l1.printList
@@ -40,8 +102,9 @@ object ReorderLinkedListV2 {
       p2.next = p1Next
       p2 = p2Next
     }
+    p1.printList
 
-    val newLL = new LinkedList()
+    val newLL : LinkedList = new LinkedList()
     newLL.head = h1
     newLL.printList
   }
@@ -49,7 +112,10 @@ object ReorderLinkedListV2 {
 
   def main(args: Array[String]): Unit = {
     // A1: Reordering a LinkedList
-    this.reorderLinkedList
+    // this.reorderLinkedList
 
+    // A2: Reverse in pairs
+    // this.reverseInPairsSwappingValues
+    this.swapNodesInPairs
   }
 }
