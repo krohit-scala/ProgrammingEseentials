a192c3125be7544b50e73875fb74140468ee4d84
krohit-scala <rohit_sun_india@yahoo.co.in>
2022-03-28 23:55:25 +0530

    TrappingRainWater

--- src/main/scala/dataStructures/impl/arrays/NearestSmallerBigger.scala
+++ src/main/scala/dataStructures/impl/arrays/NearestSmallerBigger.scala
@@ -1,31 +1,35 @@
-package dataStructures.impl.stack
+package dataStructures.impl.arrays
+
+import dataStructures.impl.stack.StackLinkedList
 
 object NearestSmallerBigger {
 
   // Prints array having nearest smaller element from left
   // for each element of the original arr
   def getNearestSmallerLeft(arr: Array[Int]): Unit = {
-    val res : Array[Int] = Array.fill(arr.length){-1}
+    val res: Array[Int] = Array.fill(arr.length) {
+      -1
+    }
     val stack = new StackLinkedList()
     var index = 0
-    for(elem <- arr){
-      if(stack.isEmpty){
+    for (elem <- arr) {
+      if (stack.isEmpty) {
         stack.push(elem)
         res(index) = -1
       }
-      else{
+      else {
         // Remove all items from stack greater than elem
-        while(!stack.isEmpty && stack.peek > elem)
+        while (!stack.isEmpty && stack.peek > elem)
           stack.pop
         // If stack is empty, res(index) = -1 and add elem to stack
         // else res(index) = stack.peek and push elem to top of the stack
-        if(stack.isEmpty)
+        if (stack.isEmpty)
           res(index) = -1
         else
           res(index) = stack.peek
         stack.push(elem)
       }
-      index +=1 
+      index += 1
     }
     println(arr.mkString(", "))
     println(res.mkString(", ") + "\n")
@@ -34,23 +38,25 @@ object NearestSmallerBigger {
   // Prints array having nearest smaller element from right
   // for each element of the original arr
   def getNearestSmallerRight(arr: Array[Int]): Unit = {
-    val res : Array[Int] = Array.fill(arr.length){-1}
+    val res: Array[Int] = Array.fill(arr.length) {
+      -1
+    }
     val stack = new StackLinkedList()
     val n = arr.length
     var index = n - 1
-    for(i <- (n-1).to(0).by(-1)){
+    for (i <- (n - 1).to(0).by(-1)) {
       val elem = arr(i)
-      if(stack.isEmpty){
+      if (stack.isEmpty) {
         stack.push(elem)
         res(i) = -1
       }
-      else{
+      else {
         // Remove all elem from stack greater than current elem
-        while(!stack.isEmpty && stack.peek > elem)
+        while (!stack.isEmpty && stack.peek > elem)
           stack.pop
 
         // Check if stack has become empty
-        if(stack.isEmpty)
+        if (stack.isEmpty)
           res(i) = -1
         else
           res(i) = stack.peek
@@ -64,24 +70,26 @@ object NearestSmallerBigger {
   // Prints array having nearest greater element from left
   // for each element of the original arr
   def getNearestGreaterLeft(arr: Array[Int]): Unit = {
-    val res : Array[Int] = Array.fill(arr.length){-1}
+    val res: Array[Int] = Array.fill(arr.length) {
+      -1
+    }
     val n = arr.length
     val stack = new StackLinkedList()
-    for(i <- 0.to(n-1)){
+    for (i <- 0.to(n - 1)) {
       val elem = arr(i)
       // Check if the stack is empty
-      if(stack.isEmpty){
+      if (stack.isEmpty) {
         res(i) = -1
         stack.push(elem)
       }
-      else{
+      else {
         // Keep popping stack until stack.top > elem
-        while(!stack.isEmpty && stack.peek < elem)
+        while (!stack.isEmpty && stack.peek < elem)
           stack.pop
 
         // If stack is empty, res(i) = -1 and stack.push(elem)
         // Else, res(i) = stack.peek, and stack.push(elem)
-        if(stack.isEmpty)
+        if (stack.isEmpty)
           res(i) = -1
         else
           res(i) = stack.peek
@@ -95,25 +103,27 @@ object NearestSmallerBigger {
   // Prints array having nearest greater element from right
   // for each element of the original arr
   def getNearestGreaterRight(arr: Array[Int]): Unit = {
-    val res : Array[Int] = Array.fill(arr.length){-1}
+    val res: Array[Int] = Array.fill(arr.length) {
+      -1
+    }
     val n = arr.length
     val stack = new StackLinkedList()
-    for(i <- (n-1).to(0).by(-1)){
+    for (i <- (n - 1).to(0).by(-1)) {
       val elem = arr(i)
 
       // Check if the stack is empty
-      if(stack.isEmpty){
+      if (stack.isEmpty) {
         res(i) = -1
         stack.push(elem)
       }
-      else{
+      else {
         // Keep removing elements from stack until stack.peek > elem
-        while(!stack.isEmpty && stack.peek < elem)
+        while (!stack.isEmpty && stack.peek < elem)
           stack.pop
 
         // Check if the stack is empty, res(i) = elem and stack.pus(elem)
         // Else, res(i) = stack.peek and stack.push(elem)
-        if(stack.isEmpty)
+        if (stack.isEmpty)
           res(i) = -1
         else
           res(i) = stack.peek
---
+++ src/main/scala/dataStructures/impl/arrays/TrappingRainWater.scala
@@ -0,0 +1,43 @@
+package dataStructures.impl.arrays
+
+object TrappingRainWater {
+  // Given n non-negative integers representing an elevation map where the width
+  // of each bar is 1, compute how much water it can trap after raining.
+  def trappingRainWater(arr: Array[Int]): Int = {
+    val n = arr.length
+    val left = Array.fill(n){-1}
+    val right = Array.fill(n){-1}
+
+    // Temp variables
+    var maxLeft = 0
+    var maxRight = 0
+    // Maximum on left
+    for(i <- 0 until n){
+      if(arr(i) > maxLeft)
+        maxLeft = arr(i)
+      left(i) = maxLeft
+    }
+    // Maximum on left
+    for(i <- (n-1) to 0 by -1){
+      if(arr(i) > maxRight)
+        maxRight = arr(i)
+      right(i) = maxRight
+    }
+
+    println(s"Orig Arr : ${arr.mkString(", ")}")
+    println(s"Max Left : ${left.mkString(", ")}")
+    println(s"Max Right : ${right.mkString(", ")}")
+
+    var res = 0
+    for(i <- 0 until n)
+      res += (math.min(right(i), left(i))) - arr(i)
+    res
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val arr : Array[Int] = Array[Int](4,2,0,3,2,5)
+    val maxWater = trappingRainWater(arr)
+    println(s"Units of water trapped: ${maxWater}")
+  }
+}
