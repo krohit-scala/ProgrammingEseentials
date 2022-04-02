230d95142d5f69c3520abcd1cbe92b3d77a4486d
krohit-scala <rohit_sun_india@yahoo.co.in>
2022-04-02 23:49:12 +0530

    MovingWindowSumOverArray - WIP

--- src/main/scala/dataStructures/impl/arrays/MovingWindowSumOverArray.scala
+++ src/main/scala/dataStructures/impl/arrays/MovingWindowSumOverArray.scala
@@ -2,7 +2,28 @@ package dataStructures.impl.arrays
 
 import dataStructures.impl.queues.QueueArray
 
+import scala.collection.mutable
+
 object MovingWindowSumOverArray {
+  def maxSlidingWindow(arr: Array[Int], k: Int): Array[Int] = {
+    if(arr.length < k || k == 0)
+      throw new Exception("Error: Invalid values supplied!")
+    else{
+      val n = arr.length
+      val res = Array.fill(n - k + 1){-1}
+
+      val q = new mutable.Queue[Int]()
+      var currMax = Int.MinValue
+      var currMin = Int.MaxValue
+      var index = 0
+      var counter = 0
+      // TBD
+      for(i <- 0 to (n - 1)){
+        // TBD
+      }
+      res
+    }
+  }
 
   def getMovingWindowSumArray(arr: Array[Int], win: Int) : Array[Int] = {
     if(arr.length < win || win == 0)
@@ -45,5 +66,11 @@ object MovingWindowSumOverArray {
     val wind = 4
     println(s"Original Array: ${arr.mkString(", ")}")
     println(s"Wind Sum Array: ${getMovingWindowSumArray(arr, wind).mkString(", ")}")
+
+    val nums = Array[Int](1,3,-1,-3,5,3,6,7)
+    val k = 3
+    var res = maxSlidingWindow(nums, k)
+    println(s"\nOriginal Array: ${nums.mkString(", ")}")
+    println(s"Wind Sum Array: ${res.mkString(", ")}")
   }
 }
