e70608dcef67281e0bfb58e04eee84c35c05ff7f
krohit-scala <rohit_sun_india@yahoo.co.in>
2022-04-09 22:41:13 +0530

    BinaryTree - Top/Bottom View of Binary Tree

--- src/main/scala/dataStructures/impl/tree/TopViewOfBinaryTree.scala
+++ src/main/scala/dataStructures/impl/tree/TopViewOfBinaryTree.scala
@@ -35,6 +35,39 @@ object TopViewOfBinaryTree {
     // Read the data from the TreeMap
     val keys = hdMap.keySet
     // Print the view
+    println(s"\nTop View:")
+    for(key <- keys)
+      println(s"HD: ${key}, Data: ${hdMap.get(key).get}")
+  }
+
+  // Get the Bottom View of the Binary Tree
+  def bottomView(root: Node) : Unit = {
+    // For Level-Order Traversal over the Binary Tree
+    var q : mutable.Queue[Pair]= mutable.Queue[Pair]()
+
+    // For keeping horizontal distance and corresponding data
+    var hdMap = TreeMap[Int, Int]()
+
+    // Inserting the root element in the Queue for Level-Order Traversal
+    q.enqueue(new Pair(0, root))
+
+    // Level-Order Traversal to populate the TreeMap for TopView
+    while(!q.isEmpty){
+      val temp = q.dequeue
+      hdMap += temp.hd -> temp.node.data
+
+      // Add left node
+      if(temp.node.left != null)
+        q.enqueue(new Pair(temp.hd-1, temp.node.left))
+      // Add right node
+      if(temp.node.right != null)
+        q.enqueue(new Pair(temp.hd+1, temp.node.right))
+    }
+
+    // Read the data from the TreeMap
+    val keys = hdMap.keySet
+    // Print the view
+    println(s"\nBottom View:")
     for(key <- keys)
       println(s"HD: ${key}, Data: ${hdMap.get(key).get}")
   }
@@ -42,5 +75,6 @@ object TopViewOfBinaryTree {
   def main(args: Array[String]): Unit = {
     val tree = BinaryTree.createTree
     topView(tree)
+    bottomView(tree)
   }
 }
