5b865ae8a4977a7d3d9d62f0b097ab1639253b19
krohit-scala <rohit_sun_india@yahoo.co.in>
2022-04-21 00:51:03 +0530

    Arrays - Array Manipulation - Prefix Sum Algo

--- src/main/scala/hackerrank/dataStructures/array/easy/ArrayManipulation.scala
+++ src/main/scala/hackerrank/dataStructures/array/easy/ArrayManipulation.scala
@@ -4,6 +4,32 @@ import scala.io.StdIn
 
 object ArrayManipulation {
   // Does that in O[N^2]
+  def arrayManipulationOptimized(n: Int, queries: Array[Array[Int]]): Long = {
+    // Write your code here
+    val data : Array[Long] = Array.fill(n+1){0}
+    var maxVal : Long = Int.MinValue
+    for(arr <- queries) {
+      // Query parameters
+      val start = arr(0)
+      val end = arr(1)
+      val delta = arr(2)
+
+      // Manage margins
+      data(start-1) += delta
+      data(end) -= delta
+    }
+
+    // Run prefix-sum algorithm
+    var tempSum : Long = 0
+    for(d <- data){
+      tempSum += d
+      if(tempSum > maxVal)
+        maxVal = tempSum
+    }
+    maxVal
+  }
+
+  // Does that in O[N^2]
   def arrayManipulation(n: Int, queries: Array[Array[Int]]): Long = {
     // Write your code here
     val data : Array[Long] = Array.fill(n){0}
@@ -30,12 +56,17 @@ object ArrayManipulation {
   }
 
   def main(args: Array[String]): Unit = {
-    val m = 3
+    println("Set number of parameters:")
+    val params = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+    val n = params(0)
+    val m = params(1)
+
     val queries = Array.ofDim[Int](m, 3)
     println("Enter the parameters:")
     for (i <- 0 until m) {
       queries(i) = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
     }
-    println(arrayManipulation(5, queries))
+    println(arrayManipulation(n, queries))
+    println(arrayManipulationOptimized(n, queries))
   }
 }
