1a4b8a0fc619e64c3b5eecafd0dc73469367038c
kumar-rohit <kumarrohitsinbox@gmail.com>
2022-05-09 23:13:23 +0530

    Kth-largest element in an array Break over coding begins

---
+++ src/main/scala/platforms/leetcode/arrays/KthLargestElement.scala
@@ -0,0 +1,18 @@
+package platforms.leetcode.arrays
+
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+object KthLargestElement {
+  def sortArray(arr: Array[Int]) : Array[Int] = {
+
+    null
+  }
+
+  def findKthLargest(nums: Array[Int], k: Int): Int = {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val arr = Array(3,2,3,1,2,4,5,5,6)
+
+  }
+}
