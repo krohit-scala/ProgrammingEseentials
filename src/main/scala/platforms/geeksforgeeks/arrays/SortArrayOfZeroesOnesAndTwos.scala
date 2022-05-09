7dce87425130aebd86aac0bc71e6828ba5c0b649
kumar-rohit <kumarrohitsinbox@gmail.com>
2022-05-10 00:29:01 +0530

    Kth-largest element in an array Break over coding begins

---
+++ src/main/scala/platforms/geeksforgeeks/arrays/SortArrayOfZeroesOnesAndTwos.scala
@@ -0,0 +1,9 @@
+package platforms.geeksforgeeks.arrays
+// https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/
+object SortArrayOfZeroesOnesAndTwos {
+
+
+  def main(args: Array[String]): Unit = {
+    var arr = Array(0, 2, 1, 2, 0)
+  }
+}
--- src/main/scala/platforms/leetcode/arrays/KthLargestElement.scala
+++ src/main/scala/platforms/leetcode/arrays/KthLargestElement.scala
@@ -2,17 +2,14 @@ package platforms.leetcode.arrays
 
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 object KthLargestElement {
-  def sortArray(arr: Array[Int]) : Array[Int] = {
-
-    null
-  }
-
-  def findKthLargest(nums: Array[Int], k: Int): Int = {
-
+  def findKthLargest(arr: Array[Int], k: Int): Int = {
+    val n = arr.length
+    val arr1 = arr.sorted
+    arr1(n-k)
   }
 
   def main(args: Array[String]): Unit = {
     val arr = Array(3,2,3,1,2,4,5,5,6)
-
+    println(s"Kth highest element in the array is: ${findKthLargest(arr, 2)}")
   }
 }
